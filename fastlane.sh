d95c1bdac01ef6e56d2414ac2e9c25133b71cd08
Aaron <qkrsogusl3@gmail.com>
2022-07-06 10:52:20 +0900

    Update fastlane.sh

--- fastlane.sh
+++ fastlane.sh
@@ -10,12 +10,14 @@ eval "$(rbenv init -)"
 # pyenv
 eval "$(pyenv init --path)"
 
+echo "--- cocoapods checking"
 cd $XCODE_ROOT && if [ -f "./Podfile" ]; then pod install; fi
 cd $WORK_DIR/$UNITY_PROJECT_ROOT
 
 cd "./unity-build-scripts/fastlane/"
 
+echo "--- bundle install"
 bundle install
 
-
+echo "--- running fastlane $PLATFORM $@"
 bundle exec fastlane $PLATFORM "$@"
